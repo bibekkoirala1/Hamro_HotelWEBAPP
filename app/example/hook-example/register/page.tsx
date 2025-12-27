146a12699e1891a8b9ada16237278f63f9aa6b31
bibekkoirala1 <bibek123koirala@gmail.com>
2025-12-27 21:34:25 +0545

    desgin register page

--- app/example/hook-example/register/page.tsx
+++ app/example/hook-example/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "../_component/register-form";
 export default function RegisterPage() {
   return (
     <div style={{ maxWidth: 400, margin: "50px auto" }}>
-      <h2>Register okiee</h2>
+      <h2>Register</h2>
       <RegisterForm />
     </div>
   );
