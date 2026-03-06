bfbbdcf007e6a2c59f3f18b1f73409fb69301a36
bibekkoirala1 <bibek123koirala@gmail.com>
2026-03-06 20:48:26 +0545

    feat:changed on search contact

--- src/components/WhyChooseUs/WhyChooseUs.jsx
+++ src/components/WhyChooseUs/WhyChooseUs.jsx
@@ -51,7 +51,6 @@ const WhyChooseUs = () => {
           </p>
         </div>
 
-        {/* cards section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
           {WhyChooseData.map((item) => {
             return (
